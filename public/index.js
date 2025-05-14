a57b9b7a210ab7c20b055762a28b1559b08e5ed5
Eric Manike Haare <manikeeric@gmail.com>
2025-05-14 23:16:38 +0000

    web 2..0

--- public/index.js
+++ public/index.js
@@ -3,6 +3,37 @@ const links = document.querySelectorAll("a");
 const navigations = document.getElementById("nav");
 const top1 = document.getElementById("top1");
 
+ let form = document.getElementById("my-form");
+  
+  async function handleSubmit(event) {
+    event.preventDefault();
+    let status = document.getElementById("my-form-status");
+    let data = new FormData(event.target);
+    fetch(event.target.action, {
+      method: form.method,
+      body: data,
+      headers: {
+          'Accept': 'application/json'
+      }
+    }).then(response => {
+      if (response.ok) {
+        status.innerHTML = "<h3>Thanks message received.<br> we will get back to you shortly!<h3>";
+        form.reset()
+      } else {
+        response.json().then(data => {
+          if (Object.hasOwn(data, 'errors')) {
+            status.innerHTML = data["errors"].map(error => error["message"]).join(", ")
+          } else {
+            status.innerHTML = "Oops! There was a problem submitting your form"
+          }
+        })
+      }
+    }).catch(error => {
+      status.innerHTML = "Oops! There was a problem submitting your form"
+    });
+  }
+  form.addEventListener("submit", handleSubmit);
+
 menubar.addEventListener('click',()=>{
     if(menubar.src.includes("images/menu1.png")){
     nav.style.display ="flex";
